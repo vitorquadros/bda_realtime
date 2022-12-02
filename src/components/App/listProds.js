cc41383b597eb5f2f5e285af16328eb56a33993c
Aluno <aluno@ifsul.com>
2022-12-02 10:29:54 -0300

    final

--- src/components/App/listProds.js
+++ src/components/App/listProds.js
@@ -1,144 +1,179 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    getOrderByChild,
-    getFilterByChild,
-    getMostExpensive,
-    getMostCheap,
-    getPriceRange
-} from '../Firebase/ProdutosDao';
-import { TableProds } from './tableProds';
-import 'materialize-css';
-import { Button, Select, Row, Col, ProgressBar, TextInput } from 'react-materialize';
-import { Icon } from 'react-materialize';
-
+  getOrderByChild,
+  getFilterByChild,
+  getMostExpensive,
+  getMostCheap,
+  getPriceRange,
+} from "../Firebase/ProdutosDao";
+import { TableProds } from "./tableProds";
+import "materialize-css";
+import {
+  Button,
+  Select,
+  Row,
+  Col,
+  ProgressBar,
+  TextInput,
+} from "react-materialize";
+import { Icon } from "react-materialize";
 
 function ListProds(props) {
+  const prods = [];
+  const [data, setData] = useState([]);
+  const [loading, setLoad] = useState(true);
+  const [order, setOrder] = useState("id_prod");
+  const [filter, setFilter] = useState("nome");
+  const [priceRange, setPriceRange] = useState(20000);
 
-    const prods = []
-    const [data, setData] = useState([])
-    const [loading, setLoad] = useState(true)
-    const [order, setOrder] = useState('id_prod')
-    const [filter, setFilter] = useState('nome')
-    const [priceRange, setPriceRange] = useState(20000)
+  useEffect(() => {
+    loadProdsFb();
+  }, [order, filter]);
 
-    useEffect(() => {
-        loadProdsFb()
-    }, [order, filter])
+  function loadProdsFb() {
+    getOrderByChild(order, props.firebase.db, receiveProds);
+    // console.log({ ordenar: order });
+  }
 
-    function loadProdsFb() {
-        getOrderByChild(order, props.firebase.db, receiveProds)
-        console.log({ 'ordenar': order })
+  function receiveProds(snap) {
+    if (snap.exists()) {
+      prods.push({ id: snap.key, ...snap.val() });
+
+      prods.length ? setData([...prods]) : setLoad(false);
     }
+  }
 
-    function receiveProds(snap) {
-        if (snap.exists()) {
-            prods.push({ 'id': snap.key, ...snap.val() })
-            console.log({ "produto": { 'id': snap.key, ...snap.val() } })
-            prods.length?setData([...prods]):setLoad(false)
-        }
-    }
+  const handlerSelectOrder = (event) => {
+    setOrder(event.target.value);
+  };
 
-    const handlerSelectOrder = (event) => {
-        setOrder(event.target.value)
-    }
+  const handlerSelectFilter = (event) => {
+    setFilter(event.target.value);
+  };
 
-    const handlerSelectFilter = (event) => {
-        setFilter(event.target.value)
-    }
+  const handlerApplyFilter = () => {
+    prods.length = 0;
+    setData([...prods]);
+    setInterval(() => setLoad(false), 3000);
+    const termo = document.getElementById("termo").value;
+    getFilterByChild(filter, termo, props.firebase.db, receiveProds);
+  };
 
-    const handlerApplyFilter = () => {
-        prods.length = 0;
-        setData([...prods])
-        setInterval(() => setLoad(false), 3000)
-        const termo = document.getElementById('termo').value
-        getFilterByChild(filter, termo, props.firebase.db, receiveProds)
-    }
+  const handlerMostExpensive = () => {
+    prods.length = 0;
+    setData([...prods]);
+    getMostExpensive(props.firebase.db, setData, prods);
+  };
 
-    const handlerMostExpensive = () => {
-        prods.length = 0;
-        setData([...prods])
-        getMostExpensive(props.firebase.db, setData, prods)
-    }
+  const handlerMostCheap = () => {
+    prods.length = 0;
+    setData([...prods]);
+    getMostCheap(props.firebase.db, receiveProds);
+  };
 
-    const handlerMostCheap = () => {
-        prods.length = 0;
-        setData([...prods])
-        getMostCheap(props.firebase.db, receiveProds)
-    }
+  const handlerPriceRange = (event) => {
+    prods.length = 0;
+    setData([...prods]);
+    setPriceRange(event.target.value);
+    if (event.target.value == 0) setLoad(false);
+    getPriceRange(event.target.value, props.firebase.db, receiveProds);
+  };
 
-    const handlerPriceRange = (event) => {
-        prods.length = 0;
-        setData([...prods])
-        setPriceRange(event.target.value)
-        if (event.target.value == 0) setLoad(false)
-        getPriceRange(event.target.value, props.firebase.db, receiveProds)
-    }
-
-    return (<div className='flex-container'>
-        <div className={'filterFormContainer'}>
-            <label htmlFor="order">Ordenar por:</label>
-            <Select id='order' name='order' onChange={handlerSelectOrder} 
-                value={order}>
-                <option value={'id_prod'} >ID</option>
-                <option value={'preco'} >Preco</option>
-                <option value={'qtd_estoque'} >Quantidade</option>
-                <option value={'nome'} >Nome</option>
+  return (
+    <div className="flex-container">
+      <div className={"filterFormContainer"}>
+        <label htmlFor="order">Ordenar por:</label>
+        <Select
+          id="order"
+          name="order"
+          onChange={handlerSelectOrder}
+          value={order}
+        >
+          <option value={"id_prod"}>ID</option>
+          <option value={"preco"}>Preco</option>
+          <option value={"qtd_estoque"}>Quantidade</option>
+          <option value={"nome"}>Nome</option>
+        </Select>
+        <div className="filterPanel">
+          <div>
+            <label htmlFor="filter">Filtrar por:</label>
+            <Select
+              id="filter"
+              name="field"
+              onChange={handlerSelectFilter}
+              value={filter}
+            >
+              <option value={"nome"}>Nome</option>
+              <option value={"descricao"}>Descricao</option>
             </Select>
-            <div className='filterPanel'>
-                <div>
-                    <label htmlFor="filter">Filtrar por:</label>
-                    <Select id='filter' name='field' onChange={handlerSelectFilter} value={filter}>
-                        <option value={'nome'} >Nome</option>
-                        <option value={'descricao'} >Descricao</option>
-                    </Select>
-                </div>
-                <div>
-                    <label htmlFor="termo"> Termo de filtro:</label>
-                    <TextInput id="termo" placeholder='Digite o termo' onChange={handlerApplyFilter} />
-                </div>
-            </div>
-            <label htmlFor="vol">Preco:</label>
-            <div className='rangeStyle'>
-                R$ 10-20k&nbsp;<input type='range'
-                    id={"price_range"}
-                    name={"price_range"}
-                    value={priceRange}
-                    max='20000'
-                    min='10'
-                    step='10'
-                    onChange={handlerPriceRange}
-                />
-                &nbsp;{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(priceRange)}
-            </div>
-            {/* <Button onClick={handlerApplyFilter}>Filtrar</Button> */}
-            <div className='btnsContainers'>
-                <Button
-                    className="waves-effect waves-light btn red"
-                    style={{ marginRight: '5px' }}
-                    onClick={handlerMostExpensive}>
-                    Mais Caros
-                    <Icon tiny left>account_balance_wallet</Icon>
-                </Button>
-                <Button
-                    className="waves-effect waves-light btn"
-                    style={{ marginRight: '5px' }}
-                    onClick={handlerMostCheap}>
-                    <Icon tiny left>savings</Icon>
-                    Mais Baratos
-                </Button>
-            </div>
+          </div>
+          <div>
+            <label htmlFor="termo"> Termo de filtro:</label>
+            <TextInput
+              id="termo"
+              placeholder="Digite o termo"
+              onChange={handlerApplyFilter}
+            />
+          </div>
         </div>
-        <div className={'tableContainer'}>
-            {(data.length > 0) ?
-                <TableProds produtos={data}></TableProds>
-                : (loading) ? <Row>
-                    <Col s={12}>
-                        <ProgressBar />
-                    </Col>
-                </Row> : <p>Sem Resultados</p>
-            }
+        <label htmlFor="vol">Preco:</label>
+        <div className="rangeStyle">
+          R$ 10-20k&nbsp;
+          <input
+            type="range"
+            id={"price_range"}
+            name={"price_range"}
+            value={priceRange}
+            max="20000"
+            min="10"
+            step="10"
+            onChange={handlerPriceRange}
+          />
+          &nbsp;
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(priceRange)}
         </div>
-    </div>);
+        {/* <Button onClick={handlerApplyFilter}>Filtrar</Button> */}
+        <div className="btnsContainers">
+          <Button
+            className="waves-effect waves-light btn red"
+            style={{ marginRight: "5px" }}
+            onClick={handlerMostExpensive}
+          >
+            Mais Caros
+            <Icon tiny left>
+              account_balance_wallet
+            </Icon>
+          </Button>
+          <Button
+            className="waves-effect waves-light btn"
+            style={{ marginRight: "5px" }}
+            onClick={handlerMostCheap}
+          >
+            <Icon tiny left>
+              savings
+            </Icon>
+            Mais Baratos
+          </Button>
+        </div>
+      </div>
+      <div className={"tableContainer"}>
+        {data.length > 0 ? (
+          <TableProds produtos={data}></TableProds>
+        ) : loading ? (
+          <Row>
+            <Col s={12}>
+              <ProgressBar />
+            </Col>
+          </Row>
+        ) : (
+          <p>Sem Resultados</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default ListProds
+export default ListProds;
--- src/components/Firebase/ProdutosDao.js
+++ src/components/Firebase/ProdutosDao.js
@@ -1,56 +1,66 @@
 import {
-    ref,
-    query,
-    orderByChild,
-    onChildAdded,
-    off,
-    endAt,
-    endBefore,
-    equalTo,
-    startAt,
-    startAfter,
-    onValue,
-    limitToFirst,
-    limitToLast
-} from "firebase/database"
+  ref,
+  query,
+  orderByChild,
+  onChildAdded,
+  off,
+  endAt,
+  endBefore,
+  equalTo,
+  startAt,
+  startAfter,
+  onValue,
+  limitToFirst,
+  limitToLast,
+} from "firebase/database";
 
-function getOrderByChild(order,db,callback){
-    //Exemplo
-    console.log(order);
-    const refDB = ref(db,'produtos/');
-    const consulta = query(refDB,orderByChild(order))
-    onChildAdded(consulta,callback)
+function getOrderByChild(order, db, callback) {
+  const refDB = ref(db, "produtos/");
+  const consulta = query(refDB, orderByChild(order));
+  onChildAdded(consulta, callback);
 }
 
-function getFilterByChild(filter,value, db,callback){
-    //implement aqui
-    console.log(filter)
+function getFilterByChild(filter, value, db, callback) {
+  const refDB = ref(db, "produtos/");
+  const consulta = query(refDB, orderByChild(filter), startAt(value));
+  onChildAdded(consulta, callback);
 }
 
-function getMostExpensive(db,setValue,list){
-    // implement aqui
-    /**
-     *     Nesta função é necessário implementar o callback,
-     * pois será necessário ordenar os resultados no cliente
-     * pelos produtos mais caros (reverso). É necessário chamar 
-     * a função setValue() e o array list passados como parametro.
-     * Para repassar os resultados do client React utiliza-se a função
-     * setValue() com os parametros [...list], ou seja, setValue([...list])
-     * onde em list deverá estar o array de produtos ordenados pelo preco
-     * mais caro. Lembrando que list é um array, use os métodos para trabalhar
-     * com arrays em JavaScript! Dica: usem reverse() ou unshift().
-     * 
-     *  */ 
+function getMostExpensive(db, setValue, list) {
+  const refDB = ref(db, "produtos/");
 
+  const consulta = query(refDB, orderByChild("preco"));
+  onChildAdded(consulta, (snapshot) => {
+    list.unshift(snapshot.val());
+    setValue([...list]);
+  });
 
+  console.table(Object.fromEntries(list));
+  //   console.log([...list]);
 }
 
-function getMostCheap(db,callback){
-    //implemente aqui
+function getMostCheap(db, callback) {
+  const refDB = ref(db, "produtos/");
+  const consulta = query(refDB, orderByChild("preco"));
+  onChildAdded(consulta, callback);
 }
 
-function getPriceRange(value, db,callback){//0--->limit
-    //implemente aqui
+function getPriceRange(value, db, callback) {
+  const refDB = ref(db, "produtos/");
+
+  const consulta = query(refDB, orderByChild("preco"), endAt(Number(value)));
+
+  onChildAdded(consulta, (snapshot) => {
+    console.log(snapshot.val().preco);
+  });
+
+  onChildAdded(consulta, callback);
 }
 
-export {getOrderByChild, getFilterByChild, getMostExpensive, getMostCheap, getPriceRange}
+export {
+  getOrderByChild,
+  getFilterByChild,
+  getMostExpensive,
+  getMostCheap,
+  getPriceRange,
+};
